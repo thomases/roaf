fc8c83d137d5692905ae010e986b47879c2b9f50
Thomas Espe <thomas.espe@gmail.com>
2025-08-01 23:33:26 +0200

    Update String
    Added code and functionality to make String return both the correct
    description and the date for the Fraksjon.

--- roaf.go
+++ roaf.go
@@ -27,23 +27,40 @@ import (
 	"github.com/joho/godotenv"
 )
 
+type FraksjonsType int
+
+const (
+	REST FraksjonsType = iota + 1
+	PAPIR
+	METALL = 17
+)
+
+var frakStrings = map[FraksjonsType]string{
+	REST:   "Restavfall",
+	PAPIR:  "Papp/Papir",
+	METALL: "Metall og glass",
+}
+
 // a representation of the structure of the data returned from the API
 type Fraksjon struct {
-	FraksjonId  int
+	FraksjonId  FraksjonsType
 	TommeDatoer []string
 }
 
 var datolst []Fraksjon
 
 func (f Fraksjon) String() string {
+	fstring, ok := frakStrings[f.FraksjonId]
+	if !ok {
+		fmt.Errorf("Ukjent fraksjonid %v", f.FraksjonId)
+	}
 	tparsed, err := time.Parse("2006-01-02T15:04:05", f.TommeDatoer[0])
 	if err != nil {
-		fmt.Errorf("Unable to stringify")
+		fmt.Errorf("Unable to stringify date")
 		return ""
 	}
-	return tparsed.Format("2006-01-02")
+	return fstring + ": " + tparsed.Format("2006-01-02")
 }
-
 
 func main() {
 	// load environment from .env
@@ -144,6 +161,6 @@ func parseResponse(res []byte) {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Restavfall: %s\n", datolst[0])
-	fmt.Printf("Papp/Papir: %s\n", datolst[1])
+	fmt.Printf("%s\n", datolst[0])
+	fmt.Printf("%s\n", datolst[1])
 }
